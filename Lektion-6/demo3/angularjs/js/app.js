658dbd2729b85b750a129c3d522adcc8e30324a9
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2019-03-19 14:56:00 +0100

    Lektion 6

--- Lektion-6/demo3/angularjs/js/app.js
+++ Lektion-6/demo3/angularjs/js/app.js
@@ -3,8 +3,6 @@ var app = angular.module("restapiApp", []);
 app.controller("restapiController", function($scope, $http) {
     $http.get('http://localhost:5000/api/products')
     .then(function(res) {    
-        console.log(res.data);
         $scope.products = res.data;
-        $scope.message = res.data.message;
     });
 })
--- Lektion-6/demo3/app.js
+++ Lektion-6/demo3/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+
+// CORS
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 // http://locahost:5000/api/products
 const productRoutes = require('./routes/products');
