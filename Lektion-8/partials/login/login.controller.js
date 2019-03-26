49525a6ba68dec8196280dc4614f3294d031f6cf
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2019-03-26 14:20:14 +0100

    Lektion 8

--- Lektion-8/partials/login/login.controller.js
+++ Lektion-8/partials/login/login.controller.js
@@ -8,6 +8,26 @@
     function loginController($location, authService, dialogService) {
 
         var vm = this;
+
+        function login() {
+            vm.dataLoading = true;
+
+            authService.Login(vm.username, vm.password, function(res) {
+                if(res.success) {
+                    authService.SetCredentials(vm.username, vm.password);
+                    $location.path("/");
+                } else {
+                    dialogService.Error(res.message);
+                    vm.dataLoading = false;
+                }
+            });
+        }
+
+        (function initController() {
+            authService.ClearCredentials();
+        })();
+
+        vm.login = login;
     }
 
 })();
