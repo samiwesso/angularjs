69e69511a8aabd262245fc0a0e0ef9f3d6eeb532
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2019-04-04 11:27:19 +0200

    Lektion 11

--- Lektion-11/js/app.js
+++ Lektion-11/js/app.js
@@ -8,16 +8,15 @@ angular
             });
         };
         
-        
-        
+        $scope.cart = [];
+        $scope.emptycart = "";
+
         $scope.products = [
             { id: 1, name: "Product 1", category: "Category 1", price: 4999, quantity: 1 },
             { id: 2, name: "Product 2", category: "Category 1", price: 5099, quantity: 1 },
             { id: 3, name: "Product 3", category: "Category 2", price: 10099, quantity: 1 },
             { id: 4, name: "Product 4", category: "Category 3", price: 599, quantity: 1 }
         ];
-
-        $scope.cart = [];
 
         $scope.addItem = function(product) {
             var found = getProductId($scope.cart, product.id);
@@ -28,5 +27,28 @@ angular
             else {
                 $scope.cart.push(angular.copy(product));
             }
+        }
+
+        $scope.removeItem = function(product) {
+            var index = $scope.cart.indexOf(product);
+            $scope.cart.splice(index, 1);
+        }
+
+        $scope.getProductCost = function(product) {
+            return product.quantity * product.price;
+        }
+
+        $scope.getTotal = function() {
+            var total = _.reduce($scope.cart, function(sum, product) {
+                return sum + $scope.getProductCost(product);
+            }, 0);
+
+            if($scope.cart.length === 0) {
+                $scope.emptycart = "Your cart is empty."
+            }
+            else {
+                $scope.emptycart = ""
+            }   
+            return total;
         }
     })
